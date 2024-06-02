c05b3837ffe43d97a5b5b0b059c03528f0497000
diyorbek-hikmatiullyev <diyorbek192004@icloud.com>
2024-06-02 17:47:44 +0500

    Initial Commit

---
+++ Planets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Planets
+//
+//  Created by Diyorbek Xikmatullayev on 02/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
